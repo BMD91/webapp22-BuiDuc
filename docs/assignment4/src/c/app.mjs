13e6394239778b2a32fafe4fa6754970f0fec7a4
Minh Duc Bui <buiminhduc2710@gmail.com>
2022-05-15 19:59:56 +0200

    long time debug but nothing is good

--- docs/assignment4/src/c/app.mjs
+++ docs/assignment4/src/c/app.mjs
@@ -1,10 +1,9 @@
 /**
  * @fileOverview  Auxiliary data management procedures
- * @author Gerd Wagner
+ * @person Gerd Wagner
  */
-import Author from "../m/Author.mjs";
-import Publisher from "../m/Publisher.mjs";
-import Book from "../m/Book.mjs";
+import Person from "../m/Person.mjs";
+import Movie from "../m/Movie.mjs";
 
 /*******************************************
  *** Auxiliary methods for testing **********
@@ -14,55 +13,63 @@ import Book from "../m/Book.mjs";
  */
 function generateTestData() {
   try {
-    Author.instances["1"] = new Author({
-      authorId: 1,
-      name: "Daniel Dennett"
+    Person.instances["1"] = new Person({
+      personId: 1,
+      name: "Stephen Frears"
     });
-    Author.instances["2"] = new Author({
-      authorId: 2,
-      name: "Douglas Hofstadter"
+    Person.instances["2"] = new Person({
+      personId: 2,
+      name: "George Lucas"
     });
-    Author.instances["3"] = new Author({
-      authorId: 3,
-      name: "Immanuel Kant"
+    Person.instances["3"] = new Person({
+      personId: 3,
+      name: "Quentin Tarantino"
     });
-    Author.saveAll();
-    Publisher.instances["Bantam Books"] = new Publisher({
-      name: "Bantam Books",
-      address: "New York, USA"
+    Person.instances["5"] = new Person({
+      personId: 5,
+      name: "Uma Thurman"
     });
-    Publisher.instances["Basic Books"] = new Publisher({
-      name: "Basic Books",
-      address: "New York, USA"
+    Person.instances["6"] = new Person({
+      personId: 6,
+      name: "John Travolta"
     });
-    Publisher.saveAll();
-    Book.instances["0553345842"] = new Book({
-      isbn: "0553345842",
-      title: "The Mind's I",
-      year: 1982,
-      authorIdRefs: [1,2],
-      publisher_id: "Bantam Books"
+    Person.instances["7"] = new Person({
+      personId: 7,
+      name: "Ewan McGregor"
     });
-    Book.instances["1463794762"] = new Book({
-      isbn: "1463794762",
-      title: "The Critique of Pure Reason",
-      year: 2011,
-      authorIdRefs: [3]
+    Person.instances["8"] = new Person({
+      personId: 8,
+      name: "Natalie Portman"
     });
-    Book.instances["1928565379"] = new Book({
-      isbn: "1928565379",
-      title: "The Critique of Practical Reason",
-      year: 2009,
-      authorIdRefs: [3]
+    Person.instances["9"] = new Person({
+      personId: 9,
+      name: "Keanu Reeves"
     });
-    Book.instances["0465030793"] = new Book({
-      isbn: "0465030793",
-      title: "I Am A Strange Loop",
-      year: 2000,
-      authorIdRefs: [2],
-      publisher_id: "Basic Books"
+    Person.saveAll();
+    
+    Movie.instances["1"] = new Movie({
+      movieId: 1,
+      title: "Pulp Fiction",
+      reDate: new Date(1994,4,12),
+      actors_id: [3,5,6],
+      director_id: "Quentin Tarantino"
     });
-    Book.saveAll();
+    Movie.instances["2"] = new Movie({
+      movieId: 2,
+      title: "Star Wars",
+      reDate: new Date(1977,4,25),
+      actors_id: [7,8],
+      director_id: "George Lucas"
+    });
+    Movie.instances["3"] = new Movie({
+      isbn: 3,
+      title: "Dangerous Liaisons",
+      reDate: new Date(1988,11,16),
+      actors_id: [9,5],
+      director_id: "Stephen Frears"
+    });
+    
+    Movie.saveAll();
   } catch (e) {
     console.log( `${e.constructor.name}: ${e.message}`);
   }
@@ -73,12 +80,10 @@ function generateTestData() {
 function clearData() {
   if (confirm( "Do you really want to delete the entire database?")) {
     try {
-      Author.instances = {};
-      localStorage["authors"] = "{}";
-      Publisher.instances = {};
-      localStorage["publishers"] = "{}";
-      Book.instances = {};
-      localStorage["books"] = "{}";
+      Person.instances = {};
+      localStorage["persons"] = "{}";
+      Movie.instances = {};
+      localStorage["movies"] = "{}";
       console.log("All data cleared.");
     } catch (e) {
       console.log( `${e.constructor.name}: ${e.message}`);
